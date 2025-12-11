a2001fd0c032220b94b366112c8dac644ed6b490
山本玲奈 <yamamotoreina@reinayamamotonoMacBook-Air.local>
2025-12-11 16:39:19 +0900

    温度チェッカー

--- hello.js
+++ hello.js
@@ -1,4 +1,15 @@
-function greet (name) {
-    console.log(`Hello, ${name}!`);
+//１.温度チェッカー
+function checkTemperature(t){
+    if (t >= 30){
+        console.log("Hot");
+    }else if(15 <= t && t < 30){
+        console.log("Warm");
+    }else{
+
+        console.log("Cold");
+    }
 }
-greet("Hikaru");
+
+checkTemperature(30);
+checkTemperature(20);
+checkTemperature(1);
